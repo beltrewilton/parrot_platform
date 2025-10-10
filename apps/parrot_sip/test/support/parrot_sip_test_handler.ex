48e509f1ac31c6df0bea8b8a4da85cc17fd0c254
byoungdale <brandonyoungdale@gmail.com>
2025-10-10 15:54:01 -0700

    removed unused test file

--- apps/parrot_sip/test/support/parrot_sip_test_handler.ex
+++
@@ -1,8 +0,0 @@
-defmodule ParrotSip.TestHandler do
-  @moduledoc """
-  Alias module for backward compatibility with tests.
-  Delegates to ParrotSip.Test.TestHandler.
-  """
-
-  defdelegate new(args \\ nil, opts \\ []), to: ParrotSip.Test.TestHandler
-end
